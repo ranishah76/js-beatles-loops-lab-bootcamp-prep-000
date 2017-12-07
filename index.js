6c9a00cf786bfde0dc53eeb0168dcb902b9b2d0a
Rani <rani76@gmail.com>
2017-12-07 11:54:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function theBeatlesPlay (musicians, instruments) {
+const variable = []
+
+for (var i = 0; i < musicians.length; i++) {
+    variable.push (musicians[i] + " plays " + instruments[i])
+}
+return variable
+}
+
+function johnLennonFacts (facts) {
+
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] += "!!!"
+    i++;
+}
+  return facts
+}
+
+function iLoveTheBeatles (i) {
+  var array = [] 
+  do {
+    array.push ('I love the Beatles!')
+    i++
+    while (i < 15)
+    return array
+  }
+
+}
