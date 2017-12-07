ceabc9b295a09d019bb13839d297a298e2434d86
Rani <rani76@gmail.com>
2017-12-07 12:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,13 @@ function johnLennonFacts (facts) {
   return facts
 }
 
-function iLoveTheBeatles (i) {
-  var array = [] 
-  do {
-    array.push ('I love the Beatles!')
-    i++
-    while (i < 15)
-    return array
-  }
-
+function (number) {
+  var [] 
+  var text = "";
+var i = 0;
+do {
+    text += "I love the Beatles ";
+    i++;
 }
-
+while (i < 15)
+}
